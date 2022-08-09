f5ddc8da7a4428d16c7dd5d370e4c0c6359d5cbd
andreyscott <andreyscott301@gmail.com>
2022-08-09 05:46:11 +0100

    made it look good

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<p className=' text-center justify-center items-center '>
+  <h1 className=' hover:text-pink-400 text-blue-600'>Hello World</h1>
+</p> 
+    </div>
+  );
+}
+
+export default App;
