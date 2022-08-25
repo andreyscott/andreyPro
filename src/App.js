ff5ccf761b3cf3a75f74a828ecbb4930ed41afb4
andreyscott <andreyscott301@gmail.com>
2022-08-25 11:05:35 +0100

    made it look

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import { Header } from './components/header/Header';
-import Nav from './components/Sidebar/Nav';
 import { Sidebar } from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <>
     <Sidebar />
-{/* <Nav /> */}
 <Header />
     </>
 
